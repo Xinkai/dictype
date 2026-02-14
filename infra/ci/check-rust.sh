25f5e4f4eb3d17943f21b63782ed432937098588
Xinkai Chen <xinkai.chen@qq.com>
2026-02-14 14:56:02 +0800

    ci: check Cargo.lock. (#3)
    * ci: check Cargo.lock.
    
    * fix lock

--- infra/ci/check-rust.sh
+++ infra/ci/check-rust.sh
@@ -6,6 +6,10 @@ SCRIPT_DIR="$(dirname -- "$0")"
 
 cd "${SCRIPT_DIR}/../.."
 
+echo "::group::check Cargo.lock..."
+cargo check --workspace --all-targets --locked
+echo "::endgroup::"
+
 echo "::group::clippy check..."
 cargo clippy --workspace --all-targets --all-features
 echo "::endgroup::"
